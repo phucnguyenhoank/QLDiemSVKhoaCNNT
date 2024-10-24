7f29d85680af8bed7b626ce91e53d98d367b8fce
Luan <vanluannuyen2004@gmail.com>
2024-10-24 16:31:25 +0700

    xong 5 phan cua luan

---
+++ QLDiemSVKhoaCNNT/FrmSinhVienTrongLopHoc.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace QLDiemSVKhoaCNNT
+{
+    partial class FrmSinhVienTrongLopHoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvDanhSachSV = new DataGridView();
+            lblSoLuongSV = new Label();
+            lblPhanTramQuaMon = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvDanhSachSV).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvDanhSachSV
+            // 
+            dgvDanhSachSV.AllowUserToAddRows = false;
+            dgvDanhSachSV.AllowUserToDeleteRows = false;
+            dgvDanhSachSV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDanhSachSV.Location = new Point(0, 97);
+            dgvDanhSachSV.Name = "dgvDanhSachSV";
+            dgvDanhSachSV.ReadOnly = true;
+            dgvDanhSachSV.RowHeadersWidth = 51;
+            dgvDanhSachSV.Size = new Size(800, 356);
+            dgvDanhSachSV.TabIndex = 0;
+            // 
+            // lblSoLuongSV
+            // 
+            lblSoLuongSV.AutoSize = true;
+            lblSoLuongSV.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            lblSoLuongSV.Location = new Point(5, 25);
+            lblSoLuongSV.Name = "lblSoLuongSV";
+            lblSoLuongSV.Size = new Size(135, 28);
+            lblSoLuongSV.TabIndex = 1;
+            lblSoLuongSV.Text = "lblSoLuongSV";
+            // 
+            // lblPhanTramQuaMon
+            // 
+            lblPhanTramQuaMon.AutoSize = true;
+            lblPhanTramQuaMon.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            lblPhanTramQuaMon.Location = new Point(418, 25);
+            lblPhanTramQuaMon.Name = "lblPhanTramQuaMon";
+            lblPhanTramQuaMon.Size = new Size(196, 28);
+            lblPhanTramQuaMon.TabIndex = 2;
+            lblPhanTramQuaMon.Text = "lblPhanTramQuaMon";
+            // 
+            // FrmSinhVienTrongLopHoc
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblPhanTramQuaMon);
+            Controls.Add(lblSoLuongSV);
+            Controls.Add(dgvDanhSachSV);
+            Name = "FrmSinhVienTrongLopHoc";
+            Text = "FrmSinhVienTrongLopHoc";
+            Load += FrmSinhVienTrongLopHoc_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvDanhSachSV).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        public DataGridView dgvDanhSachSV;
+        public Label lblSoLuongSV;
+        public Label lblPhanTramQuaMon;
+    }
+}
---
+++ QLDiemSVKhoaCNNT/FrmXemGiangVienVaLopHoc.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace QLDiemSVKhoaCNNT
+{
+    partial class FrmXemGiangVienVaLopHoc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            dgvLopHocDcPhuTrach = new DataGridView();
+            label1 = new Label();
+            cbx_giangvien = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dgvLopHocDcPhuTrach).BeginInit();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(347, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 7;
+            label2.Text = "label2";
+            // 
+            // dgvLopHocDcPhuTrach
+            // 
+            dgvLopHocDcPhuTrach.AllowUserToAddRows = false;
+            dgvLopHocDcPhuTrach.AllowUserToDeleteRows = false;
+            dgvLopHocDcPhuTrach.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvLopHocDcPhuTrach.Location = new Point(45, 53);
+            dgvLopHocDcPhuTrach.Name = "dgvLopHocDcPhuTrach";
+            dgvLopHocDcPhuTrach.ReadOnly = true;
+            dgvLopHocDcPhuTrach.RowHeadersWidth = 51;
+            dgvLopHocDcPhuTrach.Size = new Size(1048, 437);
+            dgvLopHocDcPhuTrach.TabIndex = 6;
+            dgvLopHocDcPhuTrach.DoubleClick += dgvLopHocDcPhuTrach_DoubleClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Giang vien:";
+            // 
+            // cbx_giangvien
+            // 
+            cbx_giangvien.FormattingEnabled = true;
+            cbx_giangvien.Location = new Point(133, 19);
+            cbx_giangvien.Name = "cbx_giangvien";
+            cbx_giangvien.Size = new Size(176, 28);
+            cbx_giangvien.TabIndex = 4;
+            cbx_giangvien.SelectedValueChanged += cbx_giangvien_SelectedValueChanged;
+            // 
+            // FrmXemGiangVienVaLopHoc
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1139, 509);
+            Controls.Add(label2);
+            Controls.Add(dgvLopHocDcPhuTrach);
+            Controls.Add(label1);
+            Controls.Add(cbx_giangvien);
+            Name = "FrmXemGiangVienVaLopHoc";
+            Text = "FrmXemGiangVienVaLopHoc";
+            Load += FrmXemGiangVienVaLopHoc_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvLopHocDcPhuTrach).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private DataGridView dgvLopHocDcPhuTrach;
+        private Label label1;
+        private ComboBox cbx_giangvien;
+    }
+}
