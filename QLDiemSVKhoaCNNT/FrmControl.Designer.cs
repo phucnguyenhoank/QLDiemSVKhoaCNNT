30b027f89bf96ae022c531584617c1b2279ed233
HoangPhuc <phucnguyeho@gmail.com>
2024-10-24 18:51:34 +0700

    thêm form control

--- QLDiemSVKhoaCNNT/FrmControl.Designer.cs
+++ QLDiemSVKhoaCNNT/FrmControl.Designer.cs
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmControl";
+            pnlLeft = new Panel();
+            btnLopHoc = new Button();
+            btnMonHoc = new Button();
+            btnGiangVien = new Button();
+            btnSinhVien = new Button();
+            pbxUTELogo = new PictureBox();
+            pnlTop = new Panel();
+            pnlBody = new Panel();
+            lblTitle = new Label();
+            pnlLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pbxUTELogo).BeginInit();
+            pnlTop.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlLeft
+            // 
+            pnlLeft.BackColor = Color.Navy;
+            pnlLeft.Controls.Add(btnLopHoc);
+            pnlLeft.Controls.Add(btnMonHoc);
+            pnlLeft.Controls.Add(btnGiangVien);
+            pnlLeft.Controls.Add(btnSinhVien);
+            pnlLeft.Controls.Add(pbxUTELogo);
+            pnlLeft.Dock = DockStyle.Left;
+            pnlLeft.Location = new Point(0, 0);
+            pnlLeft.Name = "pnlLeft";
+            pnlLeft.Size = new Size(144, 537);
+            pnlLeft.TabIndex = 0;
+            // 
+            // btnLopHoc
+            // 
+            btnLopHoc.BackColor = Color.FromArgb(0, 0, 64);
+            btnLopHoc.Dock = DockStyle.Top;
+            btnLopHoc.FlatStyle = FlatStyle.Flat;
+            btnLopHoc.ForeColor = Color.White;
+            btnLopHoc.Location = new Point(0, 319);
+            btnLopHoc.Name = "btnLopHoc";
+            btnLopHoc.Size = new Size(144, 52);
+            btnLopHoc.TabIndex = 4;
+            btnLopHoc.Text = "Lớp học";
+            btnLopHoc.UseVisualStyleBackColor = false;
+            btnLopHoc.Click += btnLopHoc_Click;
+            // 
+            // btnMonHoc
+            // 
+            btnMonHoc.BackColor = Color.FromArgb(0, 0, 64);
+            btnMonHoc.Dock = DockStyle.Top;
+            btnMonHoc.FlatStyle = FlatStyle.Flat;
+            btnMonHoc.ForeColor = Color.White;
+            btnMonHoc.Location = new Point(0, 267);
+            btnMonHoc.Name = "btnMonHoc";
+            btnMonHoc.Size = new Size(144, 52);
+            btnMonHoc.TabIndex = 3;
+            btnMonHoc.Text = "Môn học";
+            btnMonHoc.UseVisualStyleBackColor = false;
+            btnMonHoc.Click += btnMonHoc_Click;
+            // 
+            // btnGiangVien
+            // 
+            btnGiangVien.BackColor = Color.FromArgb(0, 0, 64);
+            btnGiangVien.Dock = DockStyle.Top;
+            btnGiangVien.FlatStyle = FlatStyle.Flat;
+            btnGiangVien.ForeColor = Color.White;
+            btnGiangVien.Location = new Point(0, 215);
+            btnGiangVien.Name = "btnGiangVien";
+            btnGiangVien.Size = new Size(144, 52);
+            btnGiangVien.TabIndex = 2;
+            btnGiangVien.Text = "Giảng viên";
+            btnGiangVien.UseVisualStyleBackColor = false;
+            btnGiangVien.Click += btnGiangVien_Click;
+            // 
+            // btnSinhVien
+            // 
+            btnSinhVien.BackColor = Color.FromArgb(0, 0, 64);
+            btnSinhVien.Dock = DockStyle.Top;
+            btnSinhVien.FlatStyle = FlatStyle.Flat;
+            btnSinhVien.ForeColor = Color.White;
+            btnSinhVien.Location = new Point(0, 163);
+            btnSinhVien.Name = "btnSinhVien";
+            btnSinhVien.Size = new Size(144, 52);
+            btnSinhVien.TabIndex = 1;
+            btnSinhVien.Text = "Sinh viên";
+            btnSinhVien.UseVisualStyleBackColor = false;
+            btnSinhVien.Click += btnSinhVien_Click;
+            // 
+            // pbxUTELogo
+            // 
+            pbxUTELogo.Dock = DockStyle.Top;
+            pbxUTELogo.Image = Properties.Resources.UTELogo;
+            pbxUTELogo.Location = new Point(0, 0);
+            pbxUTELogo.Name = "pbxUTELogo";
+            pbxUTELogo.Size = new Size(144, 163);
+            pbxUTELogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbxUTELogo.TabIndex = 0;
+            pbxUTELogo.TabStop = false;
+            pbxUTELogo.Click += pbxUTELogo_Click;
+            // 
+            // pnlTop
+            // 
+            pnlTop.BackColor = Color.Blue;
+            pnlTop.Controls.Add(lblTitle);
+            pnlTop.Dock = DockStyle.Top;
+            pnlTop.Location = new Point(144, 0);
+            pnlTop.Name = "pnlTop";
+            pnlTop.Size = new Size(851, 84);
+            pnlTop.TabIndex = 1;
+            // 
+            // pnlBody
+            // 
+            pnlBody.BackColor = Color.FromArgb(128, 128, 255);
+            pnlBody.Dock = DockStyle.Fill;
+            pnlBody.Location = new Point(144, 84);
+            pnlBody.Name = "pnlBody";
+            pnlBody.Size = new Size(851, 453);
+            pnlBody.TabIndex = 2;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.ForeColor = Color.White;
+            lblTitle.Location = new Point(67, 29);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(50, 20);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Home";
+            // 
+            // FrmControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(995, 537);
+            Controls.Add(pnlBody);
+            Controls.Add(pnlTop);
+            Controls.Add(pnlLeft);
+            Name = "FrmControl";
+            Text = "FrmControl";
+            pnlLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pbxUTELogo).EndInit();
+            pnlTop.ResumeLayout(false);
+            pnlTop.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel pnlLeft;
+        private Panel pnlTop;
+        private Panel pnlBody;
+        private Button btnSinhVien;
+        private PictureBox pbxUTELogo;
+        private Button btnLopHoc;
+        private Button btnMonHoc;
+        private Button btnGiangVien;
+        private Label lblTitle;
     }
 }
--- QLDiemSVKhoaCNNT/FrmControl.cs
+++ QLDiemSVKhoaCNNT/FrmControl.cs
@@ -12,9 +12,57 @@ namespace QLDiemSVKhoaCNNT
 {
     public partial class FrmControl : Form
     {
+        private Form currentFormChild;
         public FrmControl()
         {
             InitializeComponent();
+        }
+        private void OpenChildForm(Form childForm)
+        {
+            if (currentFormChild != null)
+            {
+                currentFormChild.Close();
+            }
+            currentFormChild = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            pnlBody.Controls.Add(childForm);
+            pnlBody.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void pbxUTELogo_Click(object sender, EventArgs e)
+        {
+            if (currentFormChild != null)
+            {
+                currentFormChild.Close();
+            }
+        }
+
+        private void btnSinhVien_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FrmQLSinhVien());
+            lblTitle.Text = "Sinh Viên";
+        }
+
+        private void btnGiangVien_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FrmQLGiaoVien());
+            lblTitle.Text = "Giảng Viên";
+        }
+
+        private void btnMonHoc_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FrmQLMonHoc());
+            lblTitle.Text = "Môn Học";
+        }
+
+        private void btnLopHoc_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FrmQLLopHoc());
+            lblTitle.Text = "Lớp Học";
         }
     }
 }
